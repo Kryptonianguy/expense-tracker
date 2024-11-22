8b5ad26de1d53b7e258c1d59e1e65703fdc2dcf9
Kryptonianguy <rohann828@gmail.com>
2024-11-22 18:01:32 +0530

    Initial Flask setup with config.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    # Register Blueprints
+    # Import and Register Blueprints
     from app.controllers.routes import main
     app.register_blueprint(main)
 
