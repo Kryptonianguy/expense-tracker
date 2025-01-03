f520ffb451f16cf95bfd894d7d230d0df5647453
Kryptonianguy <rohann828@gmail.com>
2025-01-03 13:07:56 +0530

    View Expense Page functionality Done.

--- website/forms.py
+++ website/forms.py
@@ -10,10 +10,10 @@ class AddExpenseForm(FlaskForm):
         if field.data > date.today():
             raise ValidationError('Expense date cannot be in the future.')
 
-    expense_name = StringField('Expense Name', validators=[DataRequired(), Length(max=80)])
+    expense_name = StringField('Expense Name', validators=[DataRequired(), Length(min=3, max=80)])
     expense_amount = FloatField('Amount', validators=[DataRequired(), NumberRange(min=0)])
     expense_category = StringField('Category')
     expense_date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired(), validExpenseDate])
-    description = TextAreaField('Description', validators=[DataRequired(), Length(max=150)])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(min=3, max=150)])
     submit_button = SubmitField('Save')
     # Cancel button is handled in the HTML template
